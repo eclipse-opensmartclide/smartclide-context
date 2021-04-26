cd3f5dbdb1533d7c9a26fa639a96a5f50de9b118
Purva Vyas <purva.vyas@atb-bremen.de>
2021-04-26 12:57:33 +0200

    auto generated license header info

--- context-monitoring/src/main/java/de/atb/context/monitoring/models/CustomFileBasedDataModel.java
+++ context-monitoring/src/main/java/de/atb/context/monitoring/models/CustomFileBasedDataModel.java
@@ -1,5 +1,19 @@
 package de.atb.context.monitoring.models;
 
+/*-
+ * #%L
+ * ATB Context Monitoring Core Services
+ * %%
+ * Copyright (C) 2015 - 2021 ATB – Institut für angewandte Systemtechnik Bremen GmbH
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import com.hp.hpl.jena.rdf.model.Model;
 import de.atb.context.common.Version;
 import de.atb.context.common.util.ApplicationScenario;
--- context-monitoring/src/main/java/de/atb/context/monitoring/parser/CustomFileParser.java
+++ context-monitoring/src/main/java/de/atb/context/monitoring/parser/CustomFileParser.java
@@ -1,5 +1,19 @@
 package de.atb.context.monitoring.parser;
 
+/*-
+ * #%L
+ * ATB Context Monitoring Core Services
+ * %%
+ * Copyright (C) 2015 - 2021 ATB – Institut für angewandte Systemtechnik Bremen GmbH
+ * %%
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ * #L%
+ */
+
 import de.atb.context.monitoring.config.models.DataSource;
 import de.atb.context.monitoring.config.models.InterpreterConfiguration;
 import de.atb.context.monitoring.index.Indexer;
